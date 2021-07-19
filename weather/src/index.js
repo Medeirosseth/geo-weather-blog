0adbea2badf7ed3c7cd886549915dacebc31958b
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-19 14:53:03 -0700

    add latitude state

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,13 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // this is the only time we do direct assignment to this.state
+    this.state = { lat: null};
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return<div> hi there </div>
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        /// we called setState- anytime you want to update state its setState
+        this.setState({lat: position.coords.latitude});
+
+      }
+    );
+
+  }
+
+
+  /// React says we have to define render!!
+  render() {
+    return <div> Lattitude: {this.state.lat} </div>
+  }
 }
 
 ReactDOM.render(
