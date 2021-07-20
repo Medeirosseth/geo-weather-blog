5a6c4449d40bbe677851b04734c3e281fd0a4d1f
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-19 20:26:10 -0700

    add conditional rendering for location and season

--- weather/src/components/SeasonDisplay.js
+++ weather/src/components/SeasonDisplay.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-
-const SeasonDisplay = () => {
-  return <div>Season Display</div>
+const getSeason = (lat, month) => {
+if(month > 2 && month < 9){
+ return  lat > 0 ? 'summer' : "winter";
+} else { 
+  return lat < 0 ? 'winter' : 'summer';
+}
 }
 
-export default SeasonDisplay
+
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  const text = season === 'winter' ? 'Burr, its chilly' : 'Lets hit the beach!';
+
+  return (
+    <div>
+      <h1> {text}</h1> 
+    </div>)
+}
+
+export default SeasonDisplay 
+
+//<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
--- weather/src/index.js
+++ weather/src/index.js
@@ -1,32 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import SeasonDisplay from "./components/SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // this is the only time we do direct assignment to this.state
-    this.state = { lat: null, errorMessage: '' };
+  
+  state = {lat: null, errorMessage: ''}
 
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        /// we called setState- anytime you want to update state its setState
-        this.setState({lat: position.coords.latitude});
-      },
-      err => {
-        this.setState({ errorMessage: err.message })
-      }
-    );
+  componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+          (position) => { this.setState({lat: position.coords.latitude});},
+          err => { this.setState({ errorMessage: err.message })}
+        );
   }
 
+  
   /// React says we have to define render!!
   render() {
     if(this.state.errorMessage && !this.state.late){
       return <div>Error: {this.state.errorMessage}</div>
     } else if (!this.state.errorMessage && this.state.lat){
-      return <div>latitude: {this.state.lat}</div>
+      return(
+      <SeasonDisplay lat={this.state.lat} />)
     } else {return <div>Loading</div>}
   }
 }
+
+
 
 ReactDOM.render(
   <App/>,
