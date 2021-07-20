62942370c371553d6d31fa8da61d6c971f269110
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-19 20:36:46 -0700

    add DRY code to ternary vairables

--- weather/src/components/SeasonDisplay.js
+++ weather/src/components/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: "Burr, it is cold",
+    iconName: "snowflake"
+  }
+}
+
 const getSeason = (lat, month) => {
 if(month > 2 && month < 9){
  return  lat > 0 ? 'summer' : "winter";
@@ -12,12 +23,15 @@ if(month > 2 && month < 9){
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
-  const text = season === 'winter' ? 'Burr, its chilly' : 'Lets hit the beach!';
+  const {text, iconName } = seasonConfig[season]
 
   return (
-    <div>
-      <h1> {text}</h1> 
-    </div>)
+  <div>
+    <i className={`${iconName} icon`} />
+    <h1>{text}</h1>
+    <i className={`${iconName} icon`} />
+  </div>
+  )
 }
 
 export default SeasonDisplay 
